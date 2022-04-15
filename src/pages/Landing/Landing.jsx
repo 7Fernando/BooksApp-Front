5673aec24ca2dddb177e0a89162b40bb9aa14d24
Lucia Gigena <73915741+gigenaucian@users.noreply.github.com>
2022-04-14 21:24:11 -0300

    landing fixed

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,66 +1,59 @@
-import { Stack,Heading, Spacer, Center, Image,  Text, Grid,GridItem, Flex, Box, Container,Square, Button, Wrap} from "@chakra-ui/react";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-
-
-
-
-
-const Landing = () => {
-
+import {Link} from 'react-router-dom';
+import landing from "../../assets/images/landing.jpg"
+import {Button,Flex,Heading,Image,Stack,Text,useBreakpointValue} from '@chakra-ui/react';
+  
+  export default function Landing() {
 	return (
-		
-		<Grid
-		h='100vh'
-		templateRows='repeat(2)'
-		templateColumns='repeat(2, 2fr)'
-		gap={3}
-		bg="black"
-	  	>
-
-			   <GridItem colSpan={2} bg='black' color='white'  >
-			   <Flex  bg="black" >
-				<Box p='5' mt='10'>
-		  		<Heading size='lg' color='green.400'>BookFlix</Heading>
-				</Box>
-				<Spacer />
-				<Box  p='5' mt='7'>
-		  		<Button bg='green.400' size='lg' mr='4' >Sign Up</Button>
-				</Box>
-	  			</Flex>
-				</GridItem>
-
-			<GridItem colSpan={4} bg='black' >
-			<Flex color='white' >
-	
-  			<Square bg='clack.500'   w={[200, 300, 700]}>
-		  
- 			<Text fontSize={{ base: '15px', md: '25px', lg: '38px' }}>
-The platform with more than 60 books available Online. Subscribe to our platform and start enjoying!</Text>
-
-  			</Square>
-	  		<Square bg='clack.500'  height="400px" flexWrap='wrap' w={[300, 300, 700]}>
-	  		<Square bg='clack.500'  height="50px" w={[300, 400, 800]}>
-			<Heading as="h2" fontSize={{ base: '17px', md: '30px', lg: '35px' }}>
-			Welcome to the Bookflix Community.
-  			</Heading>
-			</Square>	
-		  	<Link to='/home'>
-	  			<Button bg='green.400' size='lg' >Start to discover</Button>
-	  		</Link>
-  			</Square>
-
-	</Flex>
-				</GridItem>
-				
-				
-
-
-			
-
-	</Grid>
-
-	)
-};
-
-export default Landing;
+	  <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+		<Flex p={8} flex={1} align={'center'} justify={'center'}>
+		  <Stack spacing={6} w={'full'} maxW={'lg'}>
+			<Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+			  <Text
+				as={'span'}
+				position={'relative'}
+				_after={{
+				  content: "''",
+				  width: 'full',
+				  height: useBreakpointValue({ base: '20%', md: '30%' }),
+				  position: 'absolute',
+				  bottom: 1,
+				  left: 0,
+				  bg: 'green.400',
+				  zIndex: -1,
+				}}>
+				BOOKFLIX
+			  </Text>
+			  <br />{' '}
+			  <Text color={'green.400'} as={'span'}>
+			  Welcome to the Bookflix Community
+			  </Text>{' '}
+			</Heading>
+			<Text fontSize={{ base: 'md', lg: 'lg' }} color={'white.500'}>
+			The platform with more than 60 books available Online. Subscribe to our platform and start enjoying!
+			</Text>
+			<Stack >
+			<Link to='/home'>	
+			  <Button
+			  	size={'lg'}
+				px={6}  
+				rounded={'full'}
+				bg={'green.400'}
+				color={'white'}
+				_hover={{
+				  bg: 'green.900',
+				}}>Sing In</Button>
+				</Link>
+			</Stack>
+		  </Stack>
+		</Flex>
+		<Flex flex={1}>
+		  <Image
+		  	filter='auto' brightness='60%'
+			alt={'Login Image'}
+			objectFit={'cover'}
+			src={landing}
+ />
+		</Flex>
+	  </Stack>
+	);
+  }
