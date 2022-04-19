5df2165e287a528d578b55af35f3224bb809a22b
Your Name <Luiscabezas_@outlook.com>
2022-04-19 19:53:25 -0300

    Download button DONE

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -1,30 +1,16 @@
-import {
-  Box,
-  Flex,
-  Avatar,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  useColorMode,
-  Center,
-} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import SortByName from "../sorts/sortByName";
 import SortByScore from "../sorts/sortByScore";
 import Filter_topic from "../filter/Filter_topic";
+import { Box, Flex, Button } from "@chakra-ui/react";
 import Filter_language from "../filter/Filter_language";
 import Filter_athors from "../../components/filter/filter_athors";
-import { Link } from "react-router-dom";
+
 
 const SideBar = () => {
   return (
     <>
-      <Flex flexDir={"column"} > 
+      <Flex flexDir={"column"}>
         <Box m="5">
           <Link to="/plans">
             <Button variant={"outline"}>Checkout</Button>
