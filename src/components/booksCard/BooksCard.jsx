f8a4151c8869e51ac863f2d3064240a9615fc17a
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-04-20 22:56:50 -0300

    fix import

--- src/components/booksCard/BooksCard.jsx
+++ src/components/booksCard/BooksCard.jsx
@@ -1,19 +1,6 @@
 import React, { useEffect } from "react";
 import { Box, Stack, Image, Button, Center, Spinner } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
-import Filter_athors from "../../components/filter/filter_athors";
-import {
-  getBooks
-} from "../../redux/actions/books";
-import {
-  Box,
-  Center,   
-  Stack,
-  Image,
-  Button,
-  Spinner,   
-  Select,
-} from "@chakra-ui/react";
 import { ChevronUpIcon, ArrowDownIcon } from "@chakra-ui/icons";
 import { useAuth0 } from "@auth0/auth0-react";
 import { postUser } from "../../redux/actions/user";
