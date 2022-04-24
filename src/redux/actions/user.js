61b274d79638f23d2e66d437986a330815f41ef8
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-04-24 18:16:47 -0300

    jose

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 const url = import.meta.env.VITE_BASE_URL;
 const token = window.localStorage.getItem("token");
-const config = {
-  headers: {
-    Authorization: token,
-  },
-};
+const options = { 
+  headers: { "authorization": `Bearer ${token}` },
+}
 export const typesUser = {
     POST_USER: "POST_USER",
   };
 
   export const postUser = (user) => {
     return (dispatch) => {
-      return axios.post(`${url}/users`, user, config)
+      return axios.post(`${url}/users`, user, options)
         .then(response => {
           dispatch({
             type: typesUser.POST_USER,
