447299f4df35852e177af7bc6a0815ead1de1063
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-04-26 19:25:45 -0300

    protecion de rutas admin

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,24 +1,31 @@
 import axios from "axios";
 const url = import.meta.env.VITE_BASE_URL;
-const token = window.localStorage.getItem("token");
-const options = { 
-  headers: { "authorization": `Bearer ${token}` },
-}
-export const typesUser = {
-    POST_USER: "POST_USER",
-  };
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
 
-  export const postUser = (user) => {
-    return (dispatch) => {
-      return axios.post(`${url}/users`, user, options)
-        .then(response => {
-          dispatch({
-            type: typesUser.POST_USER,
-            payload: response.data
-          });
-        })
-        .catch(error => {
-          throw error;
+const autorizacion = {
+  headers: { authorization: `Bearer ${token}` },
+};
+const authorizationAdmin = {
+  headers: { authorization: `Bearer ${token}`, user: user },
+};
+
+export const typesUser = {
+  POST_USER: "POST_USER",
+};
+
+export const postUser = (user) => {
+  return (dispatch) => {
+    return axios
+      .post(`${url}/users`, user, authorizationAdmin)
+      .then((response) => {
+        dispatch({
+          type: typesUser.POST_USER,
+          payload: response.data,
         });
-    };
-  }
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+};
