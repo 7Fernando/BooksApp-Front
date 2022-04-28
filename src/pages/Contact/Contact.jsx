9bde78d7e6e08deff427b5a2727a8f7b31c24b06
Lucia Gigena <73915741+gigenaucian@users.noreply.github.com>
2022-04-28 15:43:53 -0300

    commited

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -31,6 +31,7 @@ import { Box,Button,Flex,FormControl,FormLabel,Heading,IconButton,Input,InputGro
         e.preventDefault(e);
         emailjs.sendForm('service_x7dun2b', 'template_be63qb1', form.current, "SHKb26CwJjN227AuP").then(res=> {alert("Mje enviado correctamente"),
         console.log(res)})
+       
     }
     
   
@@ -84,95 +85,3 @@ import { Box,Button,Flex,FormControl,FormLabel,Heading,IconButton,Input,InputGro
   export default Contact;
 
 
-
-//   export default function Contact() {
-//     const { hasCopied, onCopy } = useClipboard('bookflixemail@gmail.com');
-  
-//     return (
-//       <Flex
-//         bg={useColorModeValue('gray.100', 'gray.900')}
-//         align="center"
-//         justify="center"
-//         css={{
-//           backgroundImage: useColorModeValue(CONFETTI_LIGHT, CONFETTI_DARK),
-//           backgroundAttachment: 'fixed',
-//         }}
-//         id="contact">
-//         <Box
-//           borderRadius="lg"
-//           m={{ base: 5, md: 16, lg: 10 }}
-//           p={{ base: 5, lg: 16 }}>
-//           <Box>
-//             <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
-//               <Heading
-//                 fontSize={{
-//                   base: '6xl',
-//                   md: '5xl',
-//                 }}>
-//                 Get in Touch
-//               </Heading>
-  
-             
-//                 spacing={{ base: 4, md: 8, lg: 20 }}
-//                 direction={{ base: 'column', md: 'row' }}>
-
-//                 <Box
-//                   bg={useColorModeValue('white', 'gray.700')}
-//                   borderRadius="lg"
-//                   p={8}
-//                   color={useColorModeValue('gray.700', 'whiteAlpha.900')}
-//                   shadow="base">
-//                   <VStack spacing={5} >
-//                     <FormControl isRequired >
-//                       <FormLabel>Name</FormLabel>
-  
-//                       <InputGroup>
-//                         <InputLeftElement children={<BsPerson />} />
-//                         <Input type="text" name="name" placeholder="Your Name" />
-//                       </InputGroup>
-//                     </FormControl>
-  
-//                     <FormControl isRequired>
-//                       <FormLabel>Email</FormLabel>
-  
-//                       <InputGroup>
-//                         <InputLeftElement children={<MdOutlineEmail />} />
-//                         <Input
-//                           type="email"
-//                           name="email"
-//                           placeholder="Your Email"
-//                         />
-//                       </InputGroup>
-//                     </FormControl>
-  
-//                     <FormControl isRequired >
-//                       <FormLabel>Message</FormLabel>
-  
-//                       <Textarea
-//                         name="message"
-//                         placeholder="Your Message"
-//                         rows={6}
-//                         resize="none"
-//                       />
-//                     </FormControl>
-  
-//                     <Button
-//                       colorScheme="blue"
-//                       bg="blue.400"
-//                       color="white"
-//                       _hover={{
-//                         bg: 'blue.500',
-//                       }}
-//                       isFullWidth
-//                       onSubmit={envioMail}>
-//                       Send Message
-//                     </Button>
-//                   </VStack>
-//                 </Box>
-          
-//             </VStack>
-//           </Box>
-//         </Box>
-//       </Flex>
-//     );
-//   }
