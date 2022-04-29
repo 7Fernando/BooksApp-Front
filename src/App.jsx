60b934c3ec517d90483a523fde686a6b95989bc4
Lucia Gigena <73915741+gigenaucian@users.noreply.github.com>
2022-04-29 11:08:22 -0300

    about us

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Favorite from "./components/Favorites/Favorite";
 import EpubReader from "./components/epubReader/epubReader";
 import BookDetails from "./components/bookDetails/BookDetails";
 import CheckoutContainer from "./components/checkout/checkoutContainer";
+import About from './pages/About/About'
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/read/:id" element={<EpubReader />} />
         <Route path="/details/:id" element={<BookDetails />} />
         <Route path="/checkout/:id" element={<CheckoutContainer />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
